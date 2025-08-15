decd9a40973ab98bb9c496001e7831895057c1a8
Isabelle Stadtlober <isabellestadtlober@gmail.com>
2025-08-15 20:05:51 -0300

    Add files via upload

---
+++ python_isa/dados/dash.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import streamlit as st 
+import plotly.express as px 
+
+st.set_page_config(
+    page_title = 'vendas',
+    page_icon = '📊',
+    layout='wide'
+)
+
+tabela = pd.read_excel('vendas.xlsx')
+print(tabela)
+
+st.title('📊Dashboard de vendas')
+
+# campo de seleção e filtro dos dados
+regioes = st.multiselect('Selecione as regiões', tabela['Região'].unique())
+
+if regioes:
+    tabela = tabela[tabela['Região'].isin(regioes)] 
+
+# 2 métricas 
+#faturamento total 
+st.metric('Faturamento total', f"R${tabela['Valor Venda'].sum()}")
+
+#ticket médio
+st.metric('Ticket médio', f"R${tabela['Valor Venda'].mean()}")
+
+# gráfico faturamento por região
+st.bar_chart(tabela.groupby('Região')['Valor Venda'].sum())
+
+# gráfico faturamento por produto 
+st.bar_chart(tabela.groupby('Produto')['Valor Venda'].sum())
+
